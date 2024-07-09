7870dbe1b869e360e38007db8b2d0b2803f9f6d4
Kaan Kani İnce <kkince@newmind.ai>
2024-07-09 17:19:09 +0300

    useFetch custom hook created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UseFetchHookTest from "./components/use-fetch/test";
+
+function App() {
+    return (
+        <div className="App">
+            <UseFetchHookTest/>
+        </div>
+    );
+}
+
+export default App;
